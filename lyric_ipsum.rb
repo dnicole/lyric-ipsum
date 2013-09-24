bc3d8fc0862144ca0821e7d4d029c7c621323749
nicole pank <nicole.pank@gmail.com>
2013-09-24 10:02:08 -0500

    add handling for selection of latin text

--- lyric_ipsum.rb
+++ lyric_ipsum.rb
@@ -16,7 +16,8 @@ post '/result' do
   params[:paragraphs].to_i.times do
     @new_song = Ipsum::Song.new(params[:artist], params[:song])
     @no_song = true if @new_song.no_song?
-    @result << @new_song.random_lines
+    @result << @new_song.latin_add if params[:latin == 0]
+    @result << @new_song.random_lines if params[:latin == 1]
   end
 
   @result = @result.join(".<br><br>")
